43b05940a1ab629374711e9539c84f9508250fa0
Нурсиль <n.sultanoff.2002s@gmail.com>
2024-03-14 13:10:43 +0300

    MVP (w/out optional)

--- src/referal_service_back/referal/repository.py
+++ src/referal_service_back/referal/repository.py
@@ -1,4 +1,5 @@
-from sqlalchemy import select, update
+from fastapi import Depends
+from sqlalchemy import select, delete, update, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 
@@ -6,106 +7,88 @@ from referal.models import ReferalCode, Referalship
 from config import db
 
 
-class Repository:
-    """
-    Общий репозиторий для операций с базой данных.
-    """
-    def __init__(self, model):
-        self.model = model
+class CodeRepository:
 
-    async def insert(self, item):
-        """
-        Вставляет элемент в базу данных.
-        """
+    @staticmethod
+    async def insert(new_code: ReferalCode) -> ReferalCode:
         async with db as session:
             async with session.begin():
-                session.add(item)
+                session.add(new_code)
             await db.commit_rollback()
 
-    async def get_all(self):
-        """
-        Получает все элементы из базы данных.
-        """
+    @staticmethod
+    async def get_all():
         async with db as session:
-            query = select(self.model)
+            query = select(ReferalCode)
             result = await session.execute(query)
             return result.scalars().all()
 
-    async def get_by_id(self, id):
-        """
-        Получает элемент из базы данных по его идентификатору.
-        """
+    @staticmethod
+    async def get_by_id(id: str):
         async with db as session:
-            query = select(self.model).filter(self.model.id == id)
+            query = select(ReferalCode).filter(ReferalCode.id==id)
             result = await session.execute(query)
             return result.scalars().first()
 
-    async def get_by_some_ids(self, ids: List[str]):
-        """
-        Получает элементы из базы данных по списку идентификаторов.
-        """
+    @staticmethod
+    async def get_by_parent_id(parent_id: str):
         async with db as session:
-            query = select(self.model).filter(self.model.id.in_(ids))
+            query = select(ReferalCode).filter(ReferalCode.parent_id==parent_id, ReferalCode.is_active==True)
+            result = await session.execute(query)
+            return result.scalars().first()
+
+    @staticmethod
+    async def get_all_by_parent_id(parent_id: str):
+        async with db as session:
+            query = select(ReferalCode).filter(ReferalCode.parent_id==parent_id)
             result = await session.execute(query)
             return result.scalars().all()
 
-
-class CodeRepository(Repository):
-    """
-    Репозиторий для работы с реферальными кодами.
-    """
-    def __init__(self):
-        super().__init__(ReferalCode)
-
-    async def get_by_parent_id(self, parent_id: str):
-        """
-        Получает реферальный код по идентификатору родителя.
-        """
+    @staticmethod
+    async def get_by_code(code: str):
         async with db as session:
-            query = select(self.model).filter(
-                self.model.parent_id == parent_id, 
-                self.model.is_active == True
-            )
+            query = select(ReferalCode).filter(ReferalCode.code==code, ReferalCode.is_active==True)
             result = await session.execute(query)
             return result.scalars().first()
 
-    async def get_all_by_parent_id(self, parent_id: str):
-        """
-        Получает все реферальные коды по идентификатору родителя.
-        """
-        async with db as session:
-            query = select(self.model).filter(self.model.parent_id == parent_id)
-            result = await session.execute(query)
-            return result.scalars().all()
 
-    async def get_by_code(self, code: str):
-        """
-        Получает реферальный код по его значению.
-        """
+    @staticmethod
+    async def update_active_code(parent_id: str):
         async with db as session:
-            query = select(self.model).filter(
-                self.model.code == code, 
-                self.model.is_active == True
-            )
-            result = await session.execute(query)
-            return result.scalars().first()
-
-    async def update_active_code(self, parent_id: str):
-        """
-        Обновляет статус активности реферального кода по идентификатору родителя.
-        """
-        async with db as session:
-            query = update(self.model).where(
-                self.model.is_active == True, 
-                self.model.parent_id == parent_id
-            ).values(is_active=False)
+            query = update(ReferalCode).where(ReferalCode.is_active==True, ReferalCode.parent_id==parent_id).values(is_active=False)
             result = await session.execute(query)
             await db.commit_rollback()
 
 
-class ReferalshipRepository(Repository):
-    """
-    Репозиторий для работы с связями между реферальными кодами и пользователями.
-    """
-    def __init__(self):
-        super().__init__(Referalship)
+
+class ReferalshipRepository:
+
+    @staticmethod
+    async def insert(new_ref: Referalship) -> Referalship:
+        async with db as session:
+            async with session.begin():
+                session.add(new_ref)
+            await db.commit_rollback()
+
+
+    @staticmethod
+    async def get_all():
+        async with db as session:
+            query = select(Referalship)
+            result = await session.execute(query)
+            return result.scalars().all()
+
+
+    @staticmethod
+    async def get_by_id(id: str):
+        async with db as session:
+            query = select(Referalship).filter(Referalship.id==id)
+            result = await session.execute(query)
+            return result.scalars().first()
+
+    @staticmethod
+    async def get_by_some_ids(ids: List[str]):
+        async with db as session:
+            query = select(Referalship).filter(Referalship.code_id.in_(ids))
+            result = await session.execute(query)
+            return result.scalars().all()
